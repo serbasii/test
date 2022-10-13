ef81740b5352890f4534767cc0fc6772df77c328
SERKANERBASI <serkan.erbasi@savronik.com.tr>
2022-10-13 15:36:02 +0300

    script bugfix

--- scripts/script.js
+++ scripts/script.js
@@ -17,10 +17,7 @@ var isDev = null;
 jQuery(function () {
   // get url parameters
 
-
-  
-
-  var params = getUrlVars();
+ var params = getUrlVars();
   $.get({ url: "appsettings.json", cache: true })
     .done(function (res) {
       settings = res;
